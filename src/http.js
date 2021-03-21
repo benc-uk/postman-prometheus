8a38befd76054fd58aa2c61054e5d06fd7c5976b
Ben Coleman <benc.uk@gmail.com>
2021-03-21 10:39:22 +0000

    Adds request level metrics

---
+++ src/http.js
@@ -0,0 +1,112 @@
+/*
+  Simple compact HTTP library in vanilla Node.js
+  Ben Coleman, 2020
+*/
+
+const URL = require('url')
+
+module.exports = class HTTP {
+  constructor(base, parseResults = true, auth = null, headers = {}, checkStatus = true) {
+    this.baseUrl = base
+    this.parseResults = parseResults
+    this.checkStatus = checkStatus
+
+    if (auth) {
+      if (!auth.creds) {
+        throw 'HTTP error: auth creds must be set to `token` or `user:password`'
+      }
+      switch (auth.type.toLowerCase()) {
+        case 'bearer':
+          this.baseHeaders = { Authorization: `Bearer ${auth.creds}`, ...headers }
+          break
+        case 'basic': {
+          let basicAuthBuff = Buffer.from(auth.creds)
+          this.baseHeaders = { Authorization: `Basic ${basicAuthBuff.toString('base64')}`, ...headers }
+          break
+        }
+        default:
+          throw "HTTP error: auth type must be 'basic' or 'bearer'"
+      }
+    } else {
+      this.baseHeaders = headers
+    }
+  }
+
+  // HTTP GET wrapper
+  async get(path, headers = {}) {
+    return await this.request(path, 'GET', headers)
+  }
+
+  // HTTP POST wrapper
+  async post(path, data, contentType = 'application/json', headers = {}) {
+    const reqBody = typeof data == 'object' ? JSON.stringify(data) : data
+    return await this.request(path, 'POST', { 'Content-Type': contentType, ...headers }, reqBody)
+  }
+
+  // HTTP PUT wrapper
+  async put(path, data, contentType = 'application/json', headers = {}) {
+    const reqBody = typeof data == 'object' ? JSON.stringify(data) : data
+    return await this.request(path, 'PUT', { 'Content-Type': contentType, ...headers }, reqBody)
+  }
+
+  // HTTP DELETE wrapper
+  async delete(path, data, contentType = 'application/json', headers = {}) {
+    const reqBody = typeof data == 'object' ? JSON.stringify(data) : data
+    return await this.request(path, 'DELETE', { 'Content-Type': contentType, ...headers }, reqBody)
+  }
+
+  // Generic low level HTTP request wrapped as a promise
+  request(path, method = 'GET', headers = {}, reqBody = null) {
+    let req = URL.parse(`${this.baseUrl}${path}`)
+    req.headers = { ...this.baseHeaders, ...headers }
+    req.method = method
+
+    return new Promise((resolve, reject) => {
+      const httpLib = req.protocol && req.protocol.startsWith('https') ? require('https') : require('http')
+
+      if (reqBody) {
+        req.headers['Content-Length'] = reqBody.length
+      }
+
+      if (this.debug) {
+        console.log('### HTTP client request:', req, '\n### HTTP client body:', reqBody)
+      }
+
+      const request = httpLib.request(req, (response) => {
+        let body = []
+        response.on('data', (chunk) => body.push(chunk))
+        response.on('end', () => {
+          let data
+          try {
+            data = this.parseResults ? JSON.parse(body.join('')) : body.join('')
+          } catch (err) {
+            reject(`JSON parsing of response body failed  - ${err}`)
+          }
+
+          // Our custom response result wrapper, with Axios like fields
+          const resp = {
+            headers: response.headers,
+            status: response.statusCode,
+            statusText: response.statusMessage,
+            data,
+          }
+
+          if (this.debug) {
+            console.log(`### HTTP client response:`, resp)
+          }
+          if (this.checkStatus && (response.statusCode < 200 || response.statusCode > 299)) {
+            console.error(`Error! Request failed with status code: ${response.statusCode}`)
+            reject(resp)
+          }
+          resolve(resp)
+        })
+      })
+
+      request.on('error', (err) => reject(err))
+      if (reqBody) {
+        request.write(reqBody)
+      }
+      request.end()
+    })
+  }
+}
---
+++ src/server.js
@@ -0,0 +1,172 @@
+const newman = require('newman')
+const express = require('express')
+const fs = require('fs')
+const http = require('./http.js')
+
+let collectionFile = process.env.COLLECTION_FILE || './collection.json'
+const collectionUrl = process.env.COLLECTION_URL || ''
+const envFile = process.env.ENVIRONMENT_FILE || ''
+const port = process.env.PORT || '8080'
+const runInterval = process.env.RUN_INTERVAL || '30'
+const runIterations = process.env.RUN_ITERATIONS || '1'
+const enableBail = process.env.ENABLE_BAIL || 'false'
+const requestMetrics = process.env.ENABLE_REQUEST_METRICS || 'true'
+
+let collectionName = ''
+let resultSummary = {}
+let runCount = 0
+let iterationCount = 0
+let reqCount = 0
+
+function runCollection() {
+  logMessage(`Starting run of ${collectionFile}`)
+  newman.run(
+    {
+      collection: require(collectionFile),
+      iterationCount: parseInt(runIterations),
+      bail: enableBail == 'true',
+      environment: envFile,
+    },
+    runComplete
+  )
+}
+
+function runComplete(err, summary) {
+  if (!summary) {
+    logMessage(`ERROR! Failed to run collection, no summary was returned!`)
+    return
+  }
+
+  fs.writeFileSync('debug.tmp.json', JSON.stringify(summary))
+
+  const time = summary.run.timings.completed - summary.run.timings.started
+  logMessage(`Run complete, and took ${time}ms`)
+
+  runCount++
+  iterationCount += summary.run.stats.iterations.total
+  reqCount += summary.run.stats.requests.total
+
+  if (err) {
+    logMessage(`ERROR! Failed to run collection ${err}`)
+  }
+  resultSummary = summary
+  collectionName = summary.collection.name
+}
+
+function addMetric(metrics, name, value, type = 'gauge', labels = []) {
+  metrics += `# TYPE postman_${name} ${type}\n`
+
+  let labelsClone = [...labels]
+  labelsClone.push({ collection: collectionName })
+
+  let labelStr = ''
+  for (let label of labelsClone) {
+    let key = Object.keys(label)[0]
+    let value = Object.values(label)[0]
+    labelStr += `${key}="${value}",`
+  }
+  labelStr = labelStr.replace(/,\s*$/, '')
+
+  metrics += `postman_${name}{${labelStr}} ${value}\n\n`
+  return metrics
+}
+
+function logMessage(msg) {
+  console.log(`### ${new Date().toISOString().replace('T', ' ').substr(0, 16)} ${msg}`)
+}
+
+//
+// Entrypoint is here....
+//
+
+const app = express()
+
+app.get('/metrics', (req, res) => {
+  res.setHeader('content-type', 'text/plain; charset=utf-8; version=0.0.4')
+
+  let metricString = ''
+  try {
+    metricString = addMetric(metricString, 'lifetime_runs_total', runCount, 'counter')
+    metricString = addMetric(metricString, 'lifetime_iterations_total', iterationCount, 'counter')
+    metricString = addMetric(metricString, 'lifetime_requests_total', reqCount, 'counter')
+    metricString = addMetric(metricString, 'stats_iterations_total', resultSummary.run.stats.iterations.total)
+    metricString = addMetric(metricString, 'stats_iterations_failed', resultSummary.run.stats.iterations.failed)
+    metricString = addMetric(metricString, 'stats_requests_total', resultSummary.run.stats.requests.total)
+    metricString = addMetric(metricString, 'stats_requests_failed', resultSummary.run.stats.requests.failed)
+    metricString = addMetric(metricString, 'stats_tests_total', resultSummary.run.stats.tests.total)
+    metricString = addMetric(metricString, 'stats_tests_failed', resultSummary.run.stats.tests.failed)
+    metricString = addMetric(metricString, 'stats_test_scripts_total', resultSummary.run.stats.testScripts.total)
+    metricString = addMetric(metricString, 'stats_test_scripts_failed', resultSummary.run.stats.testScripts.failed)
+    metricString = addMetric(metricString, 'stats_assertions_total', resultSummary.run.stats.assertions.total)
+    metricString = addMetric(metricString, 'stats_assertions_failed', resultSummary.run.stats.assertions.failed)
+    metricString = addMetric(metricString, 'stats_transfered_bytes_total', resultSummary.run.transfers.responseTotal)
+    metricString = addMetric(metricString, 'stats_resp_avg', resultSummary.run.timings.responseAverage)
+    metricString = addMetric(metricString, 'stats_resp_min', resultSummary.run.timings.responseMin)
+    metricString = addMetric(metricString, 'stats_resp_max', resultSummary.run.timings.responseMax)
+
+    if (requestMetrics == 'true') {
+      for (let execution of resultSummary.run.executions) {
+        if (!execution.response) {
+          continue
+        }
+
+        const labels = [
+          {
+            // eslint-disable-next-line camelcase
+            request_name: execution.item.name,
+          },
+          {
+            iteration: execution.cursor.iteration,
+          },
+        ]
+        if (execution.response.code) {
+          metricString = addMetric(metricString, 'request_status_code', execution.response.code, 'gauge', labels)
+        }
+        if (execution.response.responseTime) {
+          metricString = addMetric(metricString, 'request_resp_time', execution.response.responseTime, 'gauge', labels)
+        }
+        if (execution.response.responseSize) {
+          metricString = addMetric(metricString, 'request_resp_size', execution.response.responseSize, 'gauge', labels)
+        }
+        if (execution.response.status) {
+          const statusOK = execution.response.status == 'OK' ? 1 : 0
+          metricString = addMetric(metricString, 'request_status_ok', statusOK, 'gauge', labels)
+        }
+      }
+    }
+
+    res.send(metricString)
+  } catch (err) {
+    res.status(500).send('No result data to show, maybe the collection has not run yet')
+  }
+})
+
+app.get('/', (req, res) => {
+  res.setHeader('content-type', 'text/plain')
+  res.status(404).send('Nothing here, try /metrics')
+})
+
+app.listen(port, async () => {
+  if (collectionUrl) {
+    logMessage(`Collection URL will be fetched and used ${collectionUrl}`)
+    try {
+      const h = new http(collectionUrl, false)
+      let resp = await h.get('')
+      fs.writeFileSync(`./downloaded-collection.tmp.json`, resp.data)
+      collectionFile = './downloaded-collection.tmp.json'
+    } catch (err) {
+      logMessage(`FATAL! Failed to download collection from URL\n ${JSON.stringify(err, null, 2)}`)
+      process.exit(1)
+    }
+  }
+  if (!fs.existsSync(collectionFile)) {
+    logMessage(`FATAL! Collection file '${collectionFile}' not found`)
+    process.exit(1)
+  }
+
+  logMessage(`Newman runner started & listening on ${port}`)
+  logMessage(`Collection will be run every ${runInterval} seconds`)
+
+  runCollection()
+  setInterval(runCollection, parseInt(runInterval * 1000))
+})
